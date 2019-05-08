8ae6e6f27870dd9b69daae2022233ead27a4021e
milkslapper <oscar.valenzuela69@gmail.com>
2019-05-08 16:48:05 -0700

    completed 1st project

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './App.css';
+import HeaderContainer from "./components/HeaderComponents/HeaderContainer";
+import CardContainer from "./components/CardComponents/CardContainer";
+
 
 const App = () => {
   return (
@@ -16,7 +19,10 @@ const App = () => {
           Don't forget to `default export` your components and import them here
           inside of this file in order to make them work.
         </strong>
+        
       </p>
+      <HeaderContainer/>
+      <CardContainer/>
     </div>
   );
 };
