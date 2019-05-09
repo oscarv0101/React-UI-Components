13de317c9f392e8524cf59b20d38d0fc71b73b33
milkslapper <oscar.valenzuela69@gmail.com>
2019-05-09 01:30:52 -0700

    clickable entire card

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -4,13 +4,15 @@ import './Card.css';
 
 const CardContent = () => {
     return (
+        <a href="https://google.com">
         <div  className="card-content">
         <a href="https://reactjs.org">
             <h2>Get Started with React</h2>
             <p>React makes it painless to create intereactive UIs. Design simple views for each state in your application.</p>
             </a>
-            <a href="https://reactjs.org">reactjs.org</a>
+            
         </div>
+        </a>
     );
 };
 
